b218ddce8817abeb48d086d3bf8161864f515d1c
Sushma Jayaram <sushmajram@gmail.com>
2024-04-24 20:04:10 +0530

    Execute valid external commands in /bin
    Invalid commands do not display error

--- wish.c
+++ wish.c
@@ -9,7 +9,7 @@
 int main(int argc, char *argv[]){
 	char *input = NULL;
     size_t len = 0;
-    int nread, rc, rc_wait;
+    int nread;
 	
 	while(1){
 		printf("wish> ");
@@ -24,22 +24,8 @@ int main(int argc, char *argv[]){
         		printf("Bye.\n");
         		exit(EXIT_SUCCESS);
         	}
-        	printf("Input: %s\n", input);
-      
-        	rc = fork();
-        	if(rc < 0){
-        		printError();
-        	} else if(rc == 0){ //child
-        		printf("child: %d\n", (int) getpid());
-        		char *myargs[2];
-        		myargs[0] = strdup("/bin/ls");
-        		myargs[1] = NULL;
-        		execv(myargs[0], myargs);
-				// any statement from here, within the block scope, should not execute
-        	} else {
-        		rc_wait = wait(NULL);
-        		printf("parent of %d (rc_wait:%d) (pid:%d)\n", rc, rc_wait, (int) getpid());
-        	}
+//        	if not built in
+			handleExternal(input);
         	
     	} else if (nread == -1){ // eof marker encountered
     		free(input);
@@ -51,4 +37,44 @@ int main(int argc, char *argv[]){
 	}
 	free(input);
 	return (0);
+}
+
+int handleExternal(char* ip){
+	// printf("Input: %s\n", ip);
+	char *buffer[BUFFER_SIZE];
+	char **proc_args;
+	int i, count = 0;
+	while(ip != NULL){
+		if(ip[0] == ' '){
+			*ip++;
+			continue;
+		}
+		buffer[count] = strsep(&ip, "\t ");
+		count++;
+	}
+	// printf("no. of tokens: %d\n", count);
+	int rc = fork();
+        	if(rc < 0){
+        		printError();
+        	} else if(rc == 0) { //child
+        		printf("child: %d\n", (int) getpid());
+        		proc_args = malloc((count+1) * sizeof(char*));
+        		proc_args[0] = malloc(6 + strlen(buffer[0]));
+        		strcpy(proc_args[0], "/bin/");
+        		strcat(proc_args[0], buffer[0]);
+        		for(i=1; i<count; i++){
+		    		proc_args[i] = strdup(buffer[i]);
+        		}
+        		proc_args[count] = NULL;
+        		for(i=0; i<count; i++){
+		    		printf("%s ", proc_args[i]);
+        		}
+        		printf("\n");
+        		execv(proc_args[0], proc_args);
+				// any statement from here, within the block scope, should not execute
+        	} else {
+        		int rc_wait = wait(NULL);
+        		// printf("parent of %d (rc_wait:%d) (pid:%d)\n", rc, rc_wait, (int) getpid());
+        	}
+	return 0;
 }
--- wish.h
+++ wish.h
@@ -1,6 +1,9 @@
 #include<stdio.h>
+#define BUFFER_SIZE 256
 
 void printError(){
 	char error_message[30] = "An error has occurred\n";
 	write(STDERR_FILENO, error_message, strlen(error_message));
 }
+
+int handleExternal(char*);
